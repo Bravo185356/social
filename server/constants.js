8fb563db06de33cae7768b3319d103e9529a4bb5
Oleg Bravo <oleg.bravo1853@gmail.com>
2023-07-08 10:09:04 +0200

    add bcrypt

---
+++ server/constants.js
@@ -0,0 +1,3 @@
+const defaultImg = "PAGuN09e71k.jpg"
+
+module.exports = defaultImg
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,23 +1,46 @@
 const db = require("../db");
+const bcrypt = require("bcrypt");
+const defaultImg = require("../constants");
 
 class AuthController {
   async createUser(req, res) {
     const { name, surname, login, password, city } = req.body;
-    await db.query(`INSERT INTO users (name, surname, login, password, city, posts) values ($1, $2, $3, $4, $5)`, 
-    [name, surname, login, password, city,]);
-  }
-  async login(req, res) {
-    const { id = null, login, password } = req.body;
-    let user;
-    if (req.body.id) {
-      user = await db.query(`SELECT * FROM users where id = $1`, [id]);
-    } else {
-      user = await db.query(`SELECT * FROM users where login = $1 AND password = $2`, [login, password]);
+    const isUserExisting = await db.query(`SELECT id FROM users where login = $1`, [login]);
+    if (isUserExisting) {
+      return res.json("Такой пользователь уже существует");
     }
-    if (user.rows.length === 0) {
-      res.json("Неверный логин или пароль");
+    const hashPassword = await bcrypt.hash(password, 7);
+    const date = new Date().toISOString();
+    await db.query(
+      `INSERT INTO users (name, surname, login, password, city, registration_date, last_visit, img) values ($1, $2, $3, $4, $5, $6, $6, $7)`,
+      [name, surname, login, hashPassword, city, date, defaultImg]
+    );
+    res.json("Пользователь успешно зарегистрирован");
+  }
+  async loginOnPageLoad(req, res) {
+    const { id } = req.body;
+    const user = await db.query(
+      `SELECT id, name, surname, city, login, img, last_visit, registration_date FROM users where id = $1`,
+      [id]
+    );
+    if (user) {
+      res.json(user.rows[0]);
     } else {
-      await res.json(user.rows[0]);
+      res.json("Не удалось авторизоваться");
+    }
+  }
+  async loginByForm(req, res) {
+    const { login, password } = req.body;
+    const hashPassword = await db.query(`SELECT password FROM users where login = $1`, [login]);
+    const match = await bcrypt.compare(password, hashPassword.rows[0].password);
+    if (match) {
+      const user = await db.query(
+        `SELECT id, name, surname, city, login, img, last_visit, registration_date FROM users where login = $1`,
+        [login]
+      );
+      res.json(user.rows[0]);
+    } else {
+      res.json("Неверный логин или пароль");
     }
   }
 }
