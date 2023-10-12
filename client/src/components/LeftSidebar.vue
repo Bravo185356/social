aa193d634844280f2bf7504c006e20a963fd6905
Oleg Bravo <oleg.bravo1853@gmail.com>
2023-10-12 08:36:46 +0200

    LeftSidebar.vue styles

--- client/src/components/LeftSidebar.vue
+++ client/src/components/LeftSidebar.vue
@@ -2,18 +2,37 @@
   <div class="sidebar-wrap">
     <div class="sidebar-content">
       <ul class="sidebar-list">
-        <li class="sidebar-item">Лента</li>
-        <li class="sidebar-item"><router-link to="/messager" class="sidebar-item">Сообщения</router-link></li>
-        <li><router-link to="/search" class="sidebar-item">Найти людей</router-link></li>
-        <li><router-link to="/friends" class="sidebar-item">Друзья</router-link></li>
+        <li class="sidebar-item">
+          <router-link to="/messager" class="sidebar-link"
+            ><img src="../assets/icons/message-square.svg" alt="message icon" />Сообщения</router-link
+          >
+        </li>
+        <li class="sidebar-item">
+          <router-link to="/search" class="sidebar-link"
+            ><img src="../assets/icons/search.svg" alt="search icon" />Найти людей</router-link
+          >
+        </li>
+        <li class="sidebar-item"><router-link to="/friends" class="sidebar-link">Друзья</router-link></li>
       </ul>
     </div>
   </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss">
+.sidebar-list {
+  display: block;
+  width: 200px;
+}
 .sidebar-item {
   cursor: pointer;
-  transition: color 0.2s ease-in;
+  &:hover {
+    background: #98fff4;
+  }
+}
+.sidebar-link {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 13px 15px;
 }
 </style>
